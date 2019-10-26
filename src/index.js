bae06ac5820619273f34d6e35caf55efbfdc8758
Michael Frenkel <michael.frenkel@outlook.com>
2019-10-25 20:50:27 -0700

    wire up Apollo client

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+
+import './style.css';
+import App from './App';
