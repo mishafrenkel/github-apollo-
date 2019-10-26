c0562613c1c321ff14d52951b8f85e697a1379f6
Michael Frenkel <michael.frenkel@outlook.com>
2019-10-25 21:08:58 -0700

    add Repository query and component

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -2,25 +2,59 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
-const GET_CURRENT_USER = gql`
-  {
-    viewer {
-      login
-      name
+import RepositoryList from '../Repository';
+import Loading from '../Loading';
+
+
+const GET_REPOSITORIES_OF_CURRENT_USER = gql`
+{
+  viewer {
+    repositories(
+      first: 5
+      orderBy: { direction: DESC, field: STARGAZERS }
+    ){
+      edges {
+        node {
+          id
+          name
+          url
+          descriptionHTML
+          primaryLanguage {
+            name
+          }
+          owner {
+            login
+             url
+          }
+          stargazers {
+            totalCount
+          }
+          viewerHasStarred
+          watchers {
+            totalCount
+          }
+          viewerSubscription
+        }
+      }
     }
   }
+}
 `;
 
 const Profile = () => (
   <Query query={GET_CURRENT_USER}>
-    {(data) => {
+    {({ data, loading }) => {
       const { viewer } = data;
 
-      return (
-        <div>
-          {viewer.name} {viewer.login}
-        </div>
-      );
+      if (!viewer) {
+        return null;
+      }
+
+      if (loading || !viewer) {
+        return <div>Loading...</div>
+      }
+
+      return <RepositoryList repositories={viewer.repositories} />;
     }}
   </Query>
 );
---
+++ src/Repository/index.js
@@ -0,0 +1,5 @@
+
+import RepositoryList from './RepositoryList';
+
+
+export default RepositoryList;
